08f20f30a36b045fbd7f460650906efb49e600aa
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-06-09 17:04:48 -0300

    testando funcionalidade dos inputs dos filtros

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Filter from './Components/Filter';
+import Produtos from './Components/Produtos'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Filter>
+        <Produtos></Produtos>
+      </Filter>
+      <p>oii</p>
     </div>
+    // <CardProduto>
+    //   <Filter></Filter>
+    //   <Carrinho></Carrinho>
+    //   <p> hello</p>
+    // </CardProduto>
   );
 }
 
---
+++ src/Components/Produtos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class Produtos extends React.Component {
+
+    state = {
+        produtos = [
+            {
+                id: 1,
+                name: "Foguete da Missão Apollo 11",
+                value: 10000.0,
+                imageUrl: "https://picsum.photos/200/200",
+            },
+            {
+                id: 2,
+                name: "Foguete da Missão Apollo 11",
+                value: 100.0,
+                imageUrl: "https://picsum.photos/200/200",
+            }
+        ]
+        
+    }
+    render() {
+        return (
+
+        )
+    }
+}
